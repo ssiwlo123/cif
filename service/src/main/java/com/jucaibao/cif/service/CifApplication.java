0f6accd1152762ef50013abd825bd0f28bf93288
shanlin_huwei <631162403@qq.com>
2017-12-02 12:05:41 +0800

    项目init

---
+++ service/src/main/java/com/jucaibao/cif/service/CifApplication.java
@@ -0,0 +1,12 @@
+package com.jucaibao.cif.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifApplication {
+	
+	public static void main(String[] args) {		
+		SpringApplication.run(CifApplication.class, args);
+	}
+}
